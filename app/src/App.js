9a83ce116907fbe3de2fed0464d9032bd46dc006
Sughit <andreisorici2007@gmail.com>
2024-10-22 17:25:14 +0300

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="parallax"></div>
+
+      <div style={{height: "1000px", backgroundColor: "red", fontSize: "36px"}}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab mollitia earum obcaecati consequatur ipsa quo expedita maiores fugiat et temporibus eos nostrum facere, doloribus molestiae perspiciatis ratione fugit dolores quisquam.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consequuntur necessitatibus repudiandae mollitia, placeat reprehenderit iusto, atque, sit repellendus natus voluptatem odio ipsam molestiae obcaecati? Quaerat aut reprehenderit inventore saepe.<br />
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis ducimus expedita magni, tempora modi sit dolore nemo unde provident tempore quos illum iure dolorum rerum harum corrupti nesciunt fugiat adipisci!<br />
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ratione dolorum asperiores ipsam. Deleniti odio est laborum maiores provident, optio fugiat reprehenderit ex quas, voluptate voluptatem amet dolore nisi recusandae!<br />
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore dignissimos labore nostrum tenetur obcaecati! Necessitatibus distinctio, voluptatem dolorum quisquam impedit nemo non enim laboriosam adipisci rerum delectus maiores quaerat illo.<br />
+      </div>
+    </>
   );
 }
 
