f3a5e19f8bcc8b1bb60ff4407c8ffd3ed63c0639
Sughit <andreisorici2007@gmail.com>
2024-10-22 18:29:44 +0300

    .

--- app/src/App.js
+++ app/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className='flex justify-center items-center'>
         <div className='rounded-xl p-3 header-box'>
           <p style={{padding: '30px'}}>
-            Medicamente pentru toată lumea.
+            <b>Medicamente pentru toată lumea.</b>
           </p>
         </div>
       </div>
@@ -19,7 +19,15 @@ function App() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br /><br />
+        <p align='center'>
+          <button className='button rounded-xl p-1'>
+            <p style={{padding: '15px'}}>
+              <b>Vezi medicamentele</b>
+            </p>
+          </button>
+        </p>
+        <br />
       </div>
     </>
   );
