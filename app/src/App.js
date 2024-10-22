db2bfb49ba62993bddf6d68d21ac3c49d4719219
Sughit <andreisorici2007@gmail.com>
2024-10-22 18:14:55 +0300

    magina principala

--- app/src/App.js
+++ app/src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="parallax"></div>
+      <div className='parallax'></div>
 
-      <div style={{height: "1000px", backgroundColor: "red", fontSize: "36px"}}>
+      <div className='flex justify-center items-center'>
+        <div className='rounded-xl p-3 header-box'>
+          <p style={{padding: '30px'}}>
+            Medicamente pentru toată lumea.
+          </p>
+        </div>
+      </div>
+      <div className='text-box'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab mollitia earum obcaecati consequatur ipsa quo expedita maiores fugiat et temporibus eos nostrum facere, doloribus molestiae perspiciatis ratione fugit dolores quisquam.<br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consequuntur necessitatibus repudiandae mollitia, placeat reprehenderit iusto, atque, sit repellendus natus voluptatem odio ipsam molestiae obcaecati? Quaerat aut reprehenderit inventore saepe.<br />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis ducimus expedita magni, tempora modi sit dolore nemo unde provident tempore quos illum iure dolorum rerum harum corrupti nesciunt fugiat adipisci!<br />
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ratione dolorum asperiores ipsam. Deleniti odio est laborum maiores provident, optio fugiat reprehenderit ex quas, voluptate voluptatem amet dolore nisi recusandae!<br />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore dignissimos labore nostrum tenetur obcaecati! Necessitatibus distinctio, voluptatem dolorum quisquam impedit nemo non enim laboriosam adipisci rerum delectus maiores quaerat illo.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
       </div>
     </>
   );
