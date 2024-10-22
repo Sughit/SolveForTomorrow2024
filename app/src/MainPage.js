f2d607a6e34fac25aeda5c616cb23cf93a574853
Sughit <andreisorici2007@gmail.com>
2024-10-22 19:49:49 +0300

    .

--- app/src/MainPage.js
+++ app/src/MainPage.js
@@ -8,11 +8,11 @@ function MainPage() {
       <div className='parallax'></div>
 
       <div className='flex justify-center items-center'>
-      <div className='rounded-xl p-3 header-box'>
-        <p style={{padding: '30px'}}>
-        <b>Medicamente pentru toată lumea.</b>
-        </p>
-      </div>
+        <div className='rounded-xl p-3 header-box'>
+          <p style={{padding: '30px'}}>
+          <b>Medicamente pentru toată lumea</b>
+          </p>
+        </div>
       </div>
       <div className='text-box'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
@@ -22,11 +22,11 @@ function MainPage() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br />
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure, unde necessitatibus cumque quis omnis reprehenderit? Mollitia quos suscipit obcaecati, minus officiis vero nulla, tempore blanditiis voluptas voluptatem doloribus tempora.<br /><br />
         <p align='center'>
-            <button className='button rounded-xl p-1'>
-              <p style={{padding: '15px'}}>
-                  <Link to='/Medicamente'><b>Vezi medicamentele</b></Link>
-              </p>
-            </button>
+          <button className='button rounded-xl p-1'>
+            <p style={{padding: '15px'}}>
+                <Link to='/Medicamente'><b>Vezi medicamentele</b></Link>
+            </p>
+          </button>
         </p>
         <br />
       </div>
